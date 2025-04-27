04f03ee217b687b118f7658a4e55496308a00678
Christopher Ferris <christopher.d.ferris@gmail.com>
2025-04-27 13:21:41 -0700

    Added script to resize too-big (for cloudflare pages) images smaller

---
+++ scripts/resize-too-big-images.sh
@@ -0,0 +1,7 @@
+find . \
+  -wholename "./content/work/*/images/*.png" \
+  -size +25M |
+# -I{} -> {} interpolates the filename piped in from find
+xargs -I{} \
+convert \
+  -resize "3000x" "{}" "{}"
