a12f0602b01b361e29c2bdc7b338545308f23458
Christopher Ferris <christopher.d.ferris@gmail.com>
2025-04-27 19:24:32 -0700

    Script for creating new artwork files

--- scripts/create-new-artwork.sh
+++ scripts/create-new-artwork.sh
@@ -1,29 +1,36 @@
-if [ "$1" = "" ] 
-then
-  echo "filename arg required"
-  exit 1
-fi
+#!/usr/bin/bash
 
-if [ "$2" = "" ] 
-then
-  echo "Title arg required"
-  exit 1
-fi
+function rename() {
+  [ "$1" = "" ] && echo "filename arg required" && exit 1;
 
+  [ "$2" = "" ] && echo "Title arg required" && exit 1;
 
-echo "creating files in './content/work/$1' for $2"
-mkdir "./content/work/$1"
-mkdir "./content/work/$1/images"
-touch "./content/work/$1/index.md"
+  [ "$3" = "" ] && echo "year arg required" && exit 1;
 
-local date=$(date -u +"%Y-%m-%dT%H:%M:%SZ");
-local currentcount = $(ls "./content/work" | wc -l)
-local weight = $(($currentcount * 10))
+  local rootdir="./content/work/$1"
 
-cat > "./content/work/$1/index.md" <<- EOM
+  [[ -d $rootdir ]] && echo "$rootdir already exists." && exit 1;
+
+  echo "creating files in '$rootdir' for $2"
+  mkdir "$rootdir"
+  mkdir "$rootdir/images/"
+  touch "$rootdir/index.md"
+
+  local date=$(date -Is);
+  local currentcount="$(ls "./content/work" | wc -l)"
+  local weight="$(($currentcount * 10))"
+
+  cat > "./content/work/$1/index.md" <<- EOM
 ---
-date: $date
-title: $2
-weight: $weight
+date: "$date"
+title: "$2"
+weight: "$weight"
+params:
+  year: "$3"
 ---
 EOM
+
+  echo "done."
+}
+
+rename $1 $2 $3
