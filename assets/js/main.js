17b9062b9c242f1a9932db232e2d678902cede39
Christopher Ferris <christopher.d.ferris@gmail.com>
2025-04-29 19:26:10 -0700

    mobile safari compatibility but better

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,33 +2,43 @@
 (function () {
   const imagesToFadeIn = document.querySelectorAll('.fade-in-on-load');
 
+  const imageHasInitializedMap = [...new Array(imagesToFadeIn.length)].map(() => false)
+  console.log("imageHasInitializedMap", imageHasInitializedMap);
+
   if (!imagesToFadeIn.length) return;
 
-  let currentImageIdx = 0;
-  let currentImage = imagesToFadeIn[currentImageIdx];
+  initializeImage(0);
 
-  initializeCurrentImage();
+  function initializeImage(idx) {
+    const img = imagesToFadeIn[idx];
+    const hasInitialized = imageHasInitializedMap[idx];
 
-  function initializeCurrentImage() {
-    currentImage.classList.add('is-hidden');
+    if (hasInitialized) return;
 
-    if (currentImage.complete) {
-      fadeInCurrentImage();
+    imageHasInitializedMap[idx] = true;
+    console.log("imageHasInitializedMap", imageHasInitializedMap);
+
+    img.classList.add('is-hidden');
+
+    if (img.complete) {
+      fadeInImage(idx);
     } else {
-      currentImage.addEventListener('load', () => {
-        fadeInCurrentImage();
+      img.addEventListener('load', () => {
+        fadeInImage(idx);
       });
 
       // for mobile safari compatibility
-      currentImage.onload = fadeInCurrentImage;
+      img.onload = () => {
+        fadeInImage(idx);
+      };
     }
   };
 
-  function fadeInCurrentImage() {
-    fadeIn(currentImage, () => {
-      if (currentImageIdx < imagesToFadeIn.length - 1) {
-        currentImage = imagesToFadeIn[++currentImageIdx];
-        initializeCurrentImage();
+  function fadeInImage(idx) {
+    const img = imagesToFadeIn[idx];
+    fadeIn(img, () => {
+      if (idx < imagesToFadeIn.length - 1) {
+        initializeImage(idx + 1);
       }
     });
   }
